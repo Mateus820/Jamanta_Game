7757e6857dd76ea8b40bc1bf4838eef0c484118e
Aluno <aluno@naverecife.local>
2019-01-27 10:08:33 -0300

    Yasumi_0.4

--- Assets/Scenes/Scripts/Brotamento.cs
+++ Assets/Scenes/Scripts/Brotamento.cs
@@ -6,17 +6,17 @@ public class Brotamento : MonoBehaviour
 {
     public GameObject target;
     public float distance;
+
+    [SerializeField] private float tempo;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObject", 2, 1.5f);
+        InvokeRepeating("SpawnObject", 2, tempo);
     }
 
     // Update is called once per frame
     void SpawnObject()
     {
-        float x = Random.Range(3.0f,6.0f);
-        float y = Random.Range(-2.0f, 2.0f);
-        Instantiate(target, new Vector3(x, y, 0), Quaternion.identity);
+        Instantiate(target, transform.position, Quaternion.identity);
     }
 }

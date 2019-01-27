115f76907fe289195f3156e4e7c9203b38c93e98
Aluno <aluno@naverecife.local>
2019-01-27 10:50:09 -0300

    NpcTalk_0.1

--- Assets/Scripts/Npc.cs
+++ Assets/Scripts/Npc.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Npc : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private string name;
+    [SerializeField] private Image keyImage;
+    private bool canTalk;
+
     void Start()
     {
-        
+        canTalk = false;
+        keyImage.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Player"){
+            canTalk = true;
+            keyImage.enabled = true;
+        }
     }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag == "Player"){
+            canTalk = false;
+            keyImage.enabled = false;
+        }
+    }
+
 }
